65033112d0d3663337b4985935ccb34ea2124e00
Anya Kundakchian <yourniceful@gmail.com>
2018-08-01 19:57:48 +0100

    Realm database exists not populated yet.

--- Relaxi/Data Model/Advice.swift
+++ Relaxi/Data Model/Advice.swift
@@ -9,15 +9,9 @@
 import Foundation
 import RealmSwift
 
-class Advice {
+class Advice: Object {
     
-    let adviceText : String
-    let adviceLiked : Bool
-    
-    //created an initialiser method that determines the initial values of these 2 properties, that will be passed on through the parameters when we create our Advice objects
-    init(text: String, isLiked: Bool) {
-        adviceText = text
-        adviceLiked = isLiked
-    }
+    @objc dynamic var adviceText : String = ""
+    @objc dynamic var adviceLiked : Bool = false
     
 }
